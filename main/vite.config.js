5f61c5c83ddf9dcf456bf238baf041b206486eee
Rob Winterburn <forestalwinters@gmail.com>
2025-02-03 16:11:06 +0000

    added vite and react

---
+++ main/vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    allowedHosts: ['5174-rwinterburn-rob-mxk9bi6cojy.ws-eu117.gitpod.io'], // Correct syntax
+    host: true, // Ensures Vite runs on all network interfaces
+  },
+});
